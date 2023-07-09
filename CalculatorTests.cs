e1929fc09f43a67b24a3ef19931eb9c903ac81c9
Animeology <joseph.nguyen19@yahoo.com>
2023-07-09 14:22:04 -0500

    Create CalculatorTests.cs
    AdditionTest tested

---
+++ CalculatorTests.cs
@@ -0,0 +1,25 @@
+﻿namespace Calculator
+{
+    public class CalculatorTests
+    {
+        static void Main(string[] args)
+        {
+            AdditionTest();
+        }
+
+        static void AdditionTest()
+        {
+            float x = 10;
+            float y = 20;
+            char mockOp = '+';
+
+            var expected = x + y;
+            var actual = CalculatorApp.Addition(x, mockOp, y);
+
+            if(actual == expected)
+            {
+                Console.WriteLine("Addition Test Succeeded");
+            }
+        }
+    }
+}
