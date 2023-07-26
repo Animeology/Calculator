c228df226ac801c873cf3966351f63f5aa2fbdcf
Animeology <joseph.nguyen19@yahoo.com>
2023-07-26 12:57:18 -0500

    Added Unit Test Solution

---
+++ CalculatorTests/CalculatorTests/AdditionTest.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorTests.CalculatorTests
+{
+    [TestClass]
+    public class AdditionTest
+    {
+        [TestMethod]
+        public void Add_Two_Numbers_ReturnCorrect()
+        {
+
+        }
+    }
+}
